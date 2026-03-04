097cc7896e568a2d49f2c083c12ac5530cff80d4
Ashu <ashutosh.mishra.royal.max@gmail.com>
2026-03-04 09:21:34 +0530

    .\8_Multiple of 3 and 7.cpp

--- 8_Multiple of 3 and 7.cpp
+++ 8_Multiple of 3 and 7.cpp
@@ -1,3 +1,4 @@
+// Write a program that takes an integer as input and determines whether it is a multiple of both 3 and 7. A number is a multiple of both if it is divisible by both numbers without leaving a remainder.
 #include <iostream>
 using namespace std;
 
