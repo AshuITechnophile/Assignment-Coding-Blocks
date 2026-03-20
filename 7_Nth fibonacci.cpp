b2ae7a5dc927fa093f20f2daf59ef2c207635a0a
Ashu <ashutosh.mishra.royal.max@gmail.com>
2026-03-21 00:05:46 +0530

    .\7_Nth fibonacci.cpp

--- 7_Nth fibonacci.cpp
+++ 7_Nth fibonacci.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 using namespace std;
-void fibonacci(int n)
+int main()
 {
-    int a = 0, b = 1;
-    int result;
-    for (int i = 2; i <= n; i++)
+    int n;
+    cin >> n;
+    int a = 0;
+    int b = 1;
+    int result = 0;
+    for (int i = 1; i <= n; i++)
     {
         result = a + b;
         a = b;
         b = result;
     }
-    cout << result;
-}
-int main()
-{
-    int n;
-    cin >> n;
-    fibonacci(n);
+    cout << a;
     return 0;
 }
